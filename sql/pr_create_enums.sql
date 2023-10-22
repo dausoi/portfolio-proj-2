3b4fa107c35540e9c77361062ddb5ae1cb2f8e3e
Son Sunt <47473184+sonsunt@users.noreply.github.com>
2023-10-22 16:59:50 +0700

    Update pr_create_enums.sql

--- sql/pr_create_enums.sql
+++ sql/pr_create_enums.sql
@@ -1,20 +1,18 @@
-create or replace procedure stg.pr_create_enums()
-language plpgsql
-as $$
-declare
-begin
-	DROP TYPE IF EXISTS stg.visitmode;
-	DROP TYPE IF EXISTS stg.wmproj;
-	DROP TYPE IF EXISTS stg.proj;
-
-	create type stg.visitmode as enum('desktop', 'mobile');
-	create type stg.wmproj as enum('advisory', 'commons', 'meta',
-								   'incubator', 'species', 'strategy',
-								   'outreach', 'usability', 'quality',
-								   'wikimania', 'wikitech');
-	create type stg.proj as enum('wikibooks', 'wiktionary', 'wikimediafoundation',
-								 'wikimedia', 'wikinews', 'wikiquote',
-								 'wikisource', 'wikiversity', 'wikivoyage',
-								 'mediawiki', 'wikidata', 'wikipedia');
-end
-$$
+DO $$ 
+BEGIN
+	CREATE TYPE stg.proj AS ENUM
+		('wikibooks', 'wiktionary', 'wikimediafoundation',
+		'wikimedia', 'wikinews', 'wikiquote',
+		'wikisource', 'wikiversity', 'wikivoyage',
+		'mediawiki', 'wikidata', 'wikipedia');
+	CREATE TYPE stg.visitmode AS ENUM
+		('desktop', 'mobile');
+	CREATE TYPE stg.wmproj AS ENUM
+		('advisory','commons','meta',
+		'incubator','species','strategy',
+		'outreach','usability','quality',
+		'wikimania','wikitech');
+EXCEPTION
+	WHEN duplicate_object THEN null;
+END $$
+;
